65985efa00a26a34e65759ecb3dbda9c8acee457
Prince Muchogo <princemuchogo@gmail.com>
2024-09-04 19:31:11 +0200

    login fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,8 +11,9 @@ async function login(credentials) {
         connectDB();
         const user = await User.findOne({ email: credentials.email });
         if (!User) throw new Error("Wrong Credentials.");
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) throw new Error("Wrong Credentials.")
+        const isMatch = await bcrypt.compare(credentials.password, user.password);
+        
+        if (!isMatch) throw new Error("Wrong Credentials.");
         return User;
 
     } catch (error) {
