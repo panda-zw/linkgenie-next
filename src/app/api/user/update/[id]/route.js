1a0484f7a06369569161277b9bd588be775c11bb
Prince Muchogo <princemuchogo@gmail.com>
2024-09-09 20:04:33 +0200

    profile update

---
+++ src/app/api/user/update/[id]/route.js
@@ -0,0 +1,24 @@
+import { connectDB } from "@/utils/connect";
+import User from "../../../../../models/User";
+import { NextResponse } from 'next/server';
+
+export async function PUT(req, { params }) {
+    try {
+        await connectDB();
+        const { id } = params; // Use 'id' instead of 'userId'
+
+        const userInfo= await req.json();
+        const user = await User.findByIdAndUpdate(id, userInfo, { new: true });
+        //console.log(user);
+
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+
+
+        return NextResponse.json({ message: "User updated successfully", user }, { status: 200 });
+    } catch (error) {
+        console.error("An error occurred:", error.message);
+        return NextResponse.json({ message: "An error occurred wwhile upadating user info", error: error.message }, { status: 500 });
+    }
+}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,39 @@
-"use client"
+"use client";
 
-import { signOut } from "next-auth/react"
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { signOut } from "next-auth/react";
 
 export default function Profile() {
-    return (
-        <div>
-            <h1>
-                Fatsoe
-            </h1>
-            <button onClick={() => signOut}>SignOut</button>
-        </div>
-    )
+  const { data: session, update } = useSession();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`/api/user/update/${session.user.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInfo }),
+      });
+
+      if (res.ok) {
+        toast.success(`user updated`);
+      } else {
+      }
+    } catch (error) {
+    } finally {
+    }
+  };
+  return (
+    <div>
+      <h1>Fatsoe</h1>
+      <button onClick={() => signOut}>SignOut</button>
+    </div>
+  );
 }
