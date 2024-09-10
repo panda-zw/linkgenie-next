4623013bdd6f7c1922aa25fed0f276bf698365cb
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-09-10 16:37:12 +0200

    wip
    middleware update

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,22 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import { URL } from 'url';
 
+export { default } from 'next-auth/middleware';
 export const config = {
-  matcher: ["/Generate", "/Community", "/credits", "/posts"],
+  matcher: ["/Generate", "/Community"],
 };
 
 export async function middleware(req) {
   console.log('Middleware is running for:', req.nextUrl.pathname);
-
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!token) {
-    const signInUrl = new URL('/auth/signin', req.nextUrl.origin);
-    signInUrl.searchParams.set('callbackUrl', req.nextUrl.pathname);
-    return NextResponse.redirect(signInUrl);
+    const signInUrl = new URL('/auth/signin', req.url);
+    signInUrl.searchParams.set('callbackUrl', encodeURIComponent(req.nextUrl.pathname));
+    return NextResponse.redirect(signInUrl.toString());
   }
 
   return NextResponse.next();
 }
+
