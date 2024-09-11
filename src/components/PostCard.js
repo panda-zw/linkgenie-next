fe805b9f5a6cd4108e33b02efe9f081b0faa7bc6
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-09-11 20:39:22 +0200

    wip
    delete a post

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -11,11 +11,13 @@ const PostCard = ({ post, handleDeletion }) => {
 
     const handlePostDeletion = async () => {
         try {
-          handleDeletion(post.id);
+            handleDeletion(post.id);
+            toast.success('Post deleted suceessfully');
         } catch (error) {
-          console.log("Failed to delete:", error);
+            toast.success('Failed to  delete post');
+            console.log("Failed to delete:", error);
         }
-      };
+    };
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(post.post).then(() => {
@@ -34,12 +36,20 @@ const PostCard = ({ post, handleDeletion }) => {
             <p className="font-normal text-gray-700 dark:text-gray-400">
                 {new Date(post.createdAt).toLocaleDateString()}
             </p>
-            <button
-                onClick={copyToClipboard}
-                className="mt-4 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            >
-                Copy Post
-            </button>
+            <div className='flex space-x-2'>
+                <button
+                    onClick={copyToClipboard}
+                    className="mt-4 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                >
+                    Copy
+                </button>
+                <button
+                    onClick={handlePostDeletion}
+                    className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-green-800"
+                >
+                    Delete
+                </button>
+            </div>
         </div>
     );
 };
