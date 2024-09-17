f3e969ea919b32701f16ec106988d605e8545c39
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-09-17 10:22:43 +0200

    feat
    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { AuthProvider } from '@/Provider';
 import './globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
   return (
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className="bg-gray-900">
         <AuthProvider>
           {children}
+          <Analytics />
           <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} />
         </AuthProvider>
       </body>
