70eafd8a96ccd9a0d2facea4e9b4d6b9315ed72c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-09-23 19:19:05 +0200

    wip
    post card

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -31,7 +31,7 @@ const PostCard = ({ post, handleDeletion }) => {
     return (
         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {truncateContent(post.post, 100)} {/* Display only 100 characters */}
+                {truncateContent(post.post, 100)}
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
                 {new Date(post.createdAt).toLocaleDateString()}
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,62 +4,62 @@ import { useSession } from 'next-auth/react';
 import PostCard from './PostCard';
 
 function Posts() {
-    const { data: session } = useSession();
-    const [posts, setPosts] = useState([]);
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
-    const handlePostDeletion = async (postId) => {
+  const handlePostDeletion = async (postId) => {
+    try {
+      const res = await fetch(`/api/posts/delete/${postId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const body = await res.json();
+
+      if (!res.ok) {
+        throw new Error("Failed to delete");
+      } else {
+        toast(`Post deleted successfully`);
+        setPosts(posts.filter((post) => post.id !== postId))
+
+      }
+    } catch (error) {
+      console.log("Failed to delete:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      if (session) {
         try {
-          const res = await fetch(`/api/posts/delete/${postId}`, {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-    
-          const body = await res.json();
-    
+          // Update the fetch URL to use query parameters
+          const res = await fetch(`/api/posts?id=${session.user.id}`);
           if (!res.ok) {
-            throw new Error("Failed to delete");
-          } else {
-            toast(`Post deleted successfully`);
-           setPosts(posts.filter((post)=>post.id !== postId))
-    
+            throw new Error('Network response was not ok');
           }
+          const data = await res.json();
+          setPosts(data);
         } catch (error) {
-          console.log("Failed to delete:", error);
+          console.error('Error fetching posts:', error);
         }
-      };
+      }
+    };
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            if (session) {
-                try {
-                    // Update the fetch URL to use query parameters
-                    const res = await fetch(`/api/posts?id=${session.user.id}`);
-                    if (!res.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const data = await res.json();
-                    setPosts(data);
-                } catch (error) {
-                    console.error('Error fetching posts:', error);
-                }
-            }
-        };
+    fetchPosts();
+  }, [session, posts]);
 
-        fetchPosts();
-    }, [session, posts]);
-
-    return (
-        <div>
-            <h1 className="text-white text-4xl mb-4 text-center font-black uppercase">Posts</h1>
-            <div className="posts-container flex flex-wrap gap-4 justify-center items-center">
-                {posts.map(post => (
-                    <PostCard key={post._id} post={post} handleDeletion={handlePostDeletion} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-black text-4xl px-28 py-10 font-mulish uppercase bg-gray-100">Posts</h1>
+      <div className="posts-container flex flex-wrap gap-4 justify-center items-center bg-gray-100">
+        {posts.map(post => (
+          <PostCard key={post._id} post={post} handleDeletion={handlePostDeletion} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Posts;
