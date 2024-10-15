6fd4451433ad88f170a1412b85689242911058d6
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-10-15 07:24:12 +0200

    fix
    absolute path on image

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>LinkGenie - Magical Link Management</title>
         <link rel="icon" href="/favicon/favicon.png" type="image/x-icon" />
-        <meta property="og:title" content="LinkGenie - Create Professional LinkedIn Content at the Click of a Button with AI." />
+        <meta property="og:title" content="LinkGenie" />
         <meta property="og:description" content="Effortlessly create engaging LinkedIn posts that showcase your unique style. With just one single click, our AI-powered tool generates content tailored to your voice, helping you stand out and make a lasting impression." />
-        <meta property="og:image" content="/landing/genie.png" />
+        <meta property="og:image" content="https://www.linkgenie.one/landing/genie.png" />
       </head>
       <body className='bg-custom-radial'>
         <AuthProvider>
